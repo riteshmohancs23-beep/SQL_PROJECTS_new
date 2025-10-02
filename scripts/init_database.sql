593ceecf9bb3cdd92453d15342dbbfde07738ba9
riteshmohancs23-beep <riteshmohan.cs23@bmsce.ac.in>
2025-10-02 20:47:13 +0530

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,14 @@
+/*
+This code below will create a new database called as DataWarehouse and 3 new schemas(folders)
+- bronze
+-silver
+-gold
+*/
+
+create database DataWarehouse
+use DataWarehouse;
+create schema bronze;
+create schema silver;
+
+create schema gold;
+
